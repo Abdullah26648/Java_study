0a1768cc9a035ad6717a960805d25a6f341c3e77
Abdullah <abdullah26648@gmail.com>
2023-11-16 23:33:02 +0300

    14.11.2023

--- src/j11_Arrays/Homeworks/Task19.java
+++ src/j11_Arrays/Homeworks/Task19.java
@@ -1,5 +1,7 @@
 package j11_Arrays.Homeworks;
 
+import java.util.Arrays;
+
 public class Task19 {
     public static void main(String[] args) {
         //Task-> Asagidaki multi dimensional array'lerin ic array'lerinde ayni index'e sahip
@@ -8,5 +10,23 @@ public class Task19 {
         int arr1[][] = {{1, 2}, {3, 4, 5}, {6}};
         int arr2[][] = {{7, 8, 9}, {10, 11}, {12}};
 
+        int maxLength = Math.max(arr1.length, arr2.length);
+        int[][] result = new int[maxLength][];
+
+        for (int i = 0; i < maxLength; i++) {
+            int[] innerArr1 = (i < arr1.length) ? arr1[i] : new int[0];
+            int[] innerArr2 = (i < arr2.length) ? arr2[i] : new int[0];
+
+            int maxInnerLength = Math.max(innerArr1.length, innerArr2.length);
+            result[i] = new int[maxInnerLength];
+
+            for (int j = 0; j < maxInnerLength; j++) {
+                int value1 = (j < innerArr1.length) ? innerArr1[j] : 0;
+                int value2 = (j < innerArr2.length) ? innerArr2[j] : 0;
+
+                result[i][j] = value1 + value2;
+            }
+        }
+        System.out.println(Arrays.deepToString(result));
     }
 }
