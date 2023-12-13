8b6d04aba65af2482c03b86f9878f6d3cc87ba25
Abdullah Ozdemir <135707357+Abdullah26648@users.noreply.github.com>
2023-12-13 20:24:40 +0300

    13.12.2023

--- src/j28_Itterators/Task02.java
+++ src/j28_Itterators/Task02.java
@@ -1,5 +1,9 @@
 package j28_Itterators;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Task02 {
     public static void main(String[] args) {
         /*  task->
@@ -9,5 +13,14 @@ public class Task02 {
         Orn : [2,13,56,23,45,14,40] istenen aralik 20 ile 40 arasi (sinirlar dahil)
         output: [23,40]
         */
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(14,25,36,47,58,69,12,45,78));
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            int currentNumber = iterator.next();
+            if (currentNumber <= 20 || currentNumber >= 40) {
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
     }
 }
--- src/j28_Itterators/Task03.java
+++ src/j28_Itterators/Task03.java
@@ -2,11 +2,19 @@ package j28_Itterators;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ListIterator;
 
 public class Task03 {
     public static void main(String[] args) {
         // Task->verilen listedeki her elementi 3 artirip print eden code create ediniz..
 
         ArrayList<Integer> sayiList = new ArrayList<>(Arrays.asList(14, 25, 36, 47, 58, 69, 12, 45, 78));
+        ListIterator<Integer> listIterator = sayiList.listIterator();
+
+        while (listIterator.hasNext()) {
+            int currentNumber = listIterator.next();
+            listIterator.set(currentNumber + 3);
+        }
+        System.out.println(sayiList);
     }
 }
